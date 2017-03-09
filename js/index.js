2e55b728b78b8abc866e803c78bbee02e8c81729
Oksana <oksana.halus@gmail.com>
2017-03-09 21:56:53 +0000

    forma

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-/*Carusel*/
-$(document).ready(function (){$('.flexslider').flexslider({animation: 'fade',controlsContainer: '.flexslider'});
-	});
-/*End Carusel*/
+//form
+$(function() {
+    $('#btn-submit').click(function(e) {
+       e.preventDefault();
+       $.ajax({
+            url: "https://formspree.io/oksana.halus@gmail.com", 
+            method: "POST",
+            data: {
+                clientName: $('#client-name').val(),
+                clientEmail: $('#client-email').val(),
+                comment: $('#comment').val()
+            },
+            dataType: "json"
+       })
+         .done(function() {
+             $('form').html('<h1>Thank you for contacting us!</h1>');
+         });
+    });    
+});
